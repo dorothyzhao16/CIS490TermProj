b75f017db4807a65d5c325c915fcec7d4384205c
Yiyuan Dorothy Zhao <zhao.dorothy33@gmail.com>
2018-12-01 22:24:38 -0500

    Update nfa.py

--- nfa.py
+++ nfa.py
@@ -1,9 +1,7 @@
-'''
-Created on Dec 1, 2018
+#Yiyuan Dorothy Zhao and Myra Zaidi
+#CIS490 Term Project Part 2: Fall 2018
 
-@author: zhaod
-'''
-
+from tkinter import *
 import auto
 import sys
 
@@ -31,45 +29,55 @@ def epsilonClosure(s): #do the epsilon closure for s set
                     m.append(d) 
     return trap
 #################################################################  
-auto.readFA("dfa.auto")
-auto.printFinAuto()
+def showDFA():
+    auto.readFA("dfa.auto")
+    auto.printFinAuto()
 
-sys.stdout.write("\n")
-
-f = open("dfa.input",'r')
-instruct = f.readline()
-
-ss = auto.getStartState()
-
-t = []
-t.append(ss)
-
-s1 = set(t)
-s1 = epsilonClosure(s1)
-
-print("For NFA: 0 = s, 1 = p, 2 = r, 3 = q. \nFor DFA: 0 = spr, 1 = sprq, 2 = prq.\n")
-print("--------------Equivalent DFA Below--------------")
-
-sys.stdout.write("Start at: " + str(list(s1)) + "\n")
-i = 0
-while instruct[i] != '.':
-    s2 = set()
-    for n in s1:
-        s = auto.lookup(n, instruct[i])
-        if not s:
-            s = set()
-        else:
-            s = set(s)
-        s2 = s2 | s  #union as sets
-    
-    #does epsilon closure below
-    s2 = epsilonClosure(s2)
-    sys.stdout.write(instruct[i] + ": " + str(list(s2)))
-    
-    if isFinal(s2):
-        sys.stdout.write(" accepted ")
-    
     sys.stdout.write("\n")
-    s1 = s2
+
+    f = open("dfa.input",'r')
+    instruct = f.readline()
+
+    ss = auto.getStartState()
+
+    t = []
+    t.append(ss)
+
+    s1 = set(t)
+    s1 = epsilonClosure(s1)
+
+#print("For NFA: 0 = s, 1 = p, 2 = r, 3 = q. \nFor DFA: 0 = spr, 1 = sprq, 2 = prq.\n")
+    print("--------------Equivalent DFA Below--------------")
+
+    sys.stdout.write("Start at: " + str(list(s1)) + "\n")
+    i = 0
+    while instruct[i] != '.':
+        s2 = set()
+        for n in s1:
+            s = auto.lookup(n, instruct[i])
+            if not s:
+                s = set()
+            else:
+                s = set(s)
+            s2 = s2 | s  #union as sets
     
-    i = i + 1 #next instruction, count is increment to go to next
+            #does epsilon closure below
+        s2 = epsilonClosure(s2)
+        sys.stdout.write(instruct[i] + ": " + str(list(s2)))
+    
+        if isFinal(s2):
+            sys.stdout.write(" accepted ")
+    
+        sys.stdout.write("\n")
+        s1 = s2
+    
+        i = i + 1 #next instruction, count is increment to go to next
+    
+master = Tk()
+Label(master, text = "For NFA: 0 = s, 1 = p, 2 = r, 3 = q. \nFor DFA: 0 = spr, 1 = sprq, 2 = prq.\n").grid(row = 0)
+
+Button(master, text='Quit', command=master.quit).grid(row = 3, column = 0, sticky = W, pady = 4)
+Button(master, text = 'Show', command = showDFA).grid(row = 3, column = 1, sticky = W, pady = 4)
+
+mainloop()
+    
